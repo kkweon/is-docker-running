69494666cda24d0cc9d1ec1e2fc1b282d9d78d03
Kyung Mo Kweon <kkweon@gmail.com>
2018-05-27 13:15:58 -0700

    [add] Hook & prettier

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -6,24 +6,24 @@ import * as util from "util";
 const exec = util.promisify(childProcess.exec);
 
 describe("findContainerByName", function() {
-    const containerName = "docker-test-postgres";
-    before(async function() {
-        const result = await exec(`docker run -d --name ${containerName} postgres:latest`);
-        if (result.stderr) throw "Failed to run docker container";
-    });
+  const containerName = "docker-test-postgres";
+  before(async function() {
+    const result = await exec(`docker run -d --name ${containerName} postgres:latest`);
+    if (result.stderr) throw "Failed to run docker container";
+  });
 
-    after(async function() {
-        const result = await exec(`docker rm -f ${containerName}`);
-        if (result.stderr) throw "Failed to run docker container";
-    });
+  after(async function() {
+    const result = await exec(`docker rm -f ${containerName}`);
+    if (result.stderr) throw "Failed to run docker container";
+  });
 
-    it("returns false when container is not found", async function() {
-        const result = await findContainerByName("no-container");
-        expect(result).to.be.false;
-    });
+  it("returns false when container is not found", async function() {
+    const result = await findContainerByName("no-container");
+    expect(result).to.be.false;
+  });
 
-    it("returns true when container is found", async function() {
-        const result = await findContainerByName(containerName);
-        expect(result).to.be.true;
-    });
+  it("returns true when container is found", async function() {
+    const result = await findContainerByName(containerName);
+    expect(result).to.be.true;
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import * as util from "util";
 const exec = util.promisify(childProcess.exec);
 
 export async function findContainerByName(name: string): Promise<boolean> {
-    try {
-        const result = await exec(`docker ps | grep -i ${name}`);
-        return result.stdout.length > 0;
-    } catch (error) {
-        return false;
-    }
+  try {
+    const result = await exec(`docker ps | grep -i ${name}`);
+    return result.stdout.length > 0;
+  } catch (error) {
+    return false;
+  }
 }
