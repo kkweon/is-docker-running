17eed37ea7eda98905dd869e8474fe3e6659e539
Kyung Mo Kweon <kkweon@gmail.com>
2018-05-27 00:40:54 -0700

    v1.0.2

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const isDockerRunning = require("is-docker-running");
 const args = process.argv.slice(2);
 const chalk = require("chalk");
@@ -7,5 +8,4 @@ args.forEach(async containerName => {
     const moduleName = chalk.blue(containerName);
     if (result) console.log(`${moduleName} is running`);
     else console.log(`${moduleName} is ${chalk.red("not")} running`);
-    process.exit(0);
 });
